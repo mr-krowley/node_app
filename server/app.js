06a6d13deaa29a9e3bd5cad41f8ef1c8e1d83f32
Viktor <midyukinv@mail.ru>
2023-04-16 10:45:12 +0300

    create user api

--- server/app.js
+++ server/app.js
@@ -1,7 +1,36 @@
+//подключение библиотек
 import express from "express"
-
+import cors from "cors"
+//включение библиотек
 const app = express()
 const PORT = 5000;
+app.use(cors());
+
+// наша база данных :)
+const users = [];
+
+//так сможем читать json
+ app.use(express.json())
+
+
+//обработчик запроса
+app.post("/user", (req, res) => {
+  const { name, psssword } = req.body;
+
+  let User = users.find((user) => user.name === name)
+  if (User) {
+    if (User.password === password) {
+      res.send("Добро пожаловать")
+    } else {
+      res.status(401).send("Неверный пароль");
+    }
+  } else {
+   //если такого пользователя нет то сощдаем его
+    users.push({ name, psssword })
+    res.status(201).send("Пользователь создан");
+  }
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`);
